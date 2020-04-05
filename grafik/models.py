8d4727338ab406d3d11624bc3ff837862f7fe7cf
monikaangerhoefer <zaba.mon@gmail.com>
2020-04-05 13:50:50 +0200

    update

--- grafik/models.py
+++ grafik/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ZdrofitClub(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+
+
+class Class(models.Model):
+    day = models.CharField(max_length=20)
+    hour = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    place = models.ForeignKey(ZdrofitClub, on_delete=models.CASCADE)
+    date = models.DateTimeField('class_date')
--- zdrofit/settings.py
+++ zdrofit/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grafik.apps.GrafikConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
